efbb46f969f49649de700d07c0b438e58e3d7e4d
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-09 16:36:57 -0800

    working on ajax

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var ejsLayouts = require("express-ejs-layouts");
 var path = require('path');
 var moment = require('moment');
 var app = express();
+var oauthSignature = require('oauth-signature');
+var n = require('nonce')();
+var request = require('request');
+var qs = require('querystring');
+var _ = require('lodash');
 // var db = require("./models");
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
@@ -21,9 +26,29 @@ app.use(function(req, res, next) {
 app.get('/', function(req, res) {
   res.render('site/home.ejs');
 });
-// gets all articles  db. is based on folder models name
-//get single article by ID
-//get new article form
-// create a new article
+app.post('/api/results', function(req, res) {
+  var baseUrl = '';
+})
+http://api.yelp.com/v2/search?location=San+Francisco&oauth_consumer_key=123&oauth_consumer_secret=456&oauth_token=789&oauth_token_secret=012
+
+app.post('/api/results', function(req, res) {
+  var baseUrl = 'http://api.yelp.com/v2/search';
+  var location = 'Seattle';
+  var consumerKey = 'KTDFSCvdnsaSQJSZjVHq2g';
+  var consumerSecret = 'f3qYQnTSZ3RnmirwNmnNQo3M2HY';
+  var authToken = 'pXYNMXdTBPTTA1aKvUSpCJa_6Zt1HmDq';
+  var authTokenSecret = 'lxc0DX92DKcSIRsAb7y36i958Cs';
+  var url = baseUrl + '?location=' + location + '&oauth_consumer_key=' + consumerKey + '&oauth_consumer_secret=' + consumerSecret + '&oauth_token=' + authToken + '&oauth_token_secret=' + authTokenSecret;
+  request(url, function(error, response, main) {
+    if (!error && response.statusCode == 200) {
+      api.parseJson(parsedMain);
+      res.send({
+        parsedMain: parsedMain
+      });
+    }
+  });
+});
+
 //listen
-app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
+module.exports = server;
