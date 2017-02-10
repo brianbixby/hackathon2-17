12f25e7c164fdfcb8cac72899475dc7d8b0fc5f1
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-10 01:07:15 -0800

    added 2 modals

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+    setTimeout(function() {
+      $('#slide-bottom-popup-short').modal('show');
+      $('#slide-bottom-popup-tall').modal('show');
+    }, 1000);
+});
+
 $(function() {
     $('.col-xs-4').matchHeight();
 });
