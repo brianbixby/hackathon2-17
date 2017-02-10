f9da9a9efc688ba45eb870c7daf162ee245b054d
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-02-09 23:57:36 -0800

    style on navbar w/ if statements

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.use(function(req, res, next) {
   res.locals.moment = moment;
+  res.locals.currentUser = req.user;
   next();
 });
 
